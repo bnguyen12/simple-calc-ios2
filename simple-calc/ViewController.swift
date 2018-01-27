ba3ca82ca5fb3bce5d7f61c6ae3aefd12762f5f3
Benny Nguyen <benny.nguyenx@gmail.com>
2018-01-27 00:42:58 -0800

    Add features for fact, count, and avg onto the calculator

--- simple-calc/ViewController.swift
+++ simple-calc/ViewController.swift
@@ -34,11 +34,42 @@ class ViewController: UIViewController {
   //Calculates the operation when the equals button is pressed using the inBar array of strings
   @IBAction func calculate(_ sender: UIButton) {
     if !inBar.isEmpty {
-      var text: String
+      var text: String = ""
       var firstNum: Int = Int(inBar[0])!
       let op: String = inBar[1]
-      let secondNum: Int = Int(inBar[2])!
+      var secondNum: Int = 0
+      if inBar.count >= 3 {
+        secondNum = Int(inBar[2])!
+      }
       switch op {
+      case "fact":
+        if firstNum == 0 {
+          text = "0"
+        } else {
+          var sum: Int = 1
+          for num in 1...firstNum {
+            sum *= num
+          }
+          text = "\(sum)"
+        }
+      case "count":
+        var sum: Int = 0
+        for index in 0...inBar.count - 1 {
+          if Int(inBar[index]) != nil {
+            sum += 1
+          }
+        }
+        text = "\(sum)"
+      case "avg":
+        var sum: Int = 0
+        var totalNums: Int = 0
+        for index in 0...inBar.count - 1 {
+          if Int(inBar[index]) != nil {
+            sum += Int(inBar[index])!
+            totalNums += 1
+          }
+        }
+        text = "\(sum / totalNums)"
       case "+":
         text = "\(firstNum + secondNum)"
       case "-":
@@ -53,7 +84,7 @@ class ViewController: UIViewController {
         }
         text = "\(firstNum)"
       default:
-        text = ""
+        text = "error!"
       }
       changeText.text = text
       inBar.removeAll()
